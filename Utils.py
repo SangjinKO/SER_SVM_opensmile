9b1922682c4975850d879d4d19f745dd6e81df77
SangjinKO <kosang234@gmail.com>
2020-03-07 15:11:33 -0400

    update

--- Utils.py
+++ Utils.py
@@ -1,15 +1,5 @@
-from typing import Tuple
-import numpy as np
-import matplotlib.pyplot as plt
-from keras.models import model_from_json
 from sklearn.externals import joblib
-import librosa
-import librosa.display
-import scipy.io.wavfile as wav
-import pyaudio
-import wave
 
-from Config import Config
 
 def load_model(load_model_name: str):
 
@@ -17,78 +7,3 @@ def load_model(load_model_name: str):
     model = joblib.load(model_path)
 
     return model
-
-def plotCurve(train, val, title: str, y_label: str):
-    plt.plot(train)
-    plt.plot(val)
-    plt.title(title)
-    plt.ylabel(y_label)
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.show()
-
-
-def playAudio(file_path: str):
-
-    p = pyaudio.PyAudio()
-    f = wave.open(file_path, 'rb')
-    stream = p.open(format = p.get_format_from_width(f.getsampwidth()),
-                    channels = f.getnchannels(),
-                    rate = f.getframerate(),
-                    output = True)
-    data = f.readframes(f.getparams()[3])
-    stream.write(data)
-    stream.stop_stream()
-    stream.close()
-    f.close()
-    
-
-def Radar(data_prob):
-
-    angles = np.linspace(0, 2 * np.pi, len(Config.CLASS_LABELS), endpoint = False)
-    data = np.concatenate((data_prob, [data_prob[0]]))
-    angles = np.concatenate((angles, [angles[0]]))
-
-    fig = plt.figure()
-
-    # polar
-    ax = fig.add_subplot(111, polar = True)
-    ax.plot(angles, data, 'bo-', linewidth=2)
-    ax.fill(angles, data, facecolor='r', alpha=0.25)
-    ax.set_thetagrids(angles * 180 / np.pi, Config.CLASS_LABELS)
-    ax.set_title("Emotion Recognition", va = 'bottom')
-
-    ax.set_rlim(0, 1)
-
-    ax.grid(True)
-    # plt.ion()
-    plt.show()
-    # plt.pause(4)
-    # plt.close()
-
-
-def Waveform(file_path: str):
-    data, sampling_rate = librosa.load(file_path)
-    plt.figure(figsize=(15, 5))
-    librosa.display.waveplot(data, sr = sampling_rate)
-    plt.show()
-
-def Spectrogram(file_path: str):
-    sr, x = wav.read(file_path)
-
-    # step: 10ms, window: 30ms
-    nstep = int(sr * 0.01)
-    nwin  = int(sr * 0.03)
-    nfft = nwin
-    window = np.hamming(nwin)
-
-    nn = range(nwin, len(x), nstep)
-    X = np.zeros( (len(nn), nfft//2) )
-
-    for i,n in enumerate(nn):
-        xseg = x[n-nwin:n]
-        z = np.fft.fft(window * xseg, nfft)
-        X[i,:] = np.log(np.abs(z[:nfft//2]))
-
-    plt.imshow(X.T, interpolation = 'nearest', origin = 'lower', aspect = 'auto')
-    plt.show()
--- main.py
+++ main.py
@@ -3,10 +3,8 @@ import Opensmile_Feature #Feature Extraction
 from ML_Model import SVM_Model, MLP_Model # SVM, MLP - Train & Evaluate
 from Config import Config #File Path
 
-from Utils import load_model, Radar, playAudio, Waveform, Spectrogram #Audio Data Utils
 
-
-print ("TEST-SVM using opensmile for SAVEE")
+print ("TEST-SVM using opensmile for RAVEE")
 print ("FEATURE: IS10_paraling")
 
 
@@ -19,19 +17,6 @@ def Train(save_model_name: str):
     model.save_model(save_model_name)
 
     return model
-
-def Predict(model, file_path: str):
-    file_path = os.path.dirname(os.path.abspath(__file__)) + '/' + file_path
-    Opensmile_Feature.get_data(file_path, Config.PREDICT_FEATURE_PATH_OPENSMILE, train=False)
-    test_feature = Opensmile_Feature.load_feature(Config.PREDICT_FEATURE_PATH_OPENSMILE, train=False)
-
-    result = model.predict(test_feature)
-    result_prob = model.predict_proba(test_feature)[0]
-    print('Recogntion: ', Config.CLASS_LABELS[int(result)])
-    print('Probability: ', result_prob)
-    # Radar(result_prob)
-    # Waveform(file_path)
-    # Spectrogram(file_path)
 
 ## Data Labeling
 # emotions = {
@@ -61,8 +46,3 @@ def Predict(model, file_path: str):
 ## Trainig & Validating
 Train("SVM_LIB")
 
-
-## Prediction (each file)
-# file_path = "file_name"
-# model = load_model("SVM_LIB")
-# Predict(model, file_path)
